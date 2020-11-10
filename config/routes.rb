101f4af9ac656de5d4d4b06fd854ef9b59e78b32
Juan Garcia <juagarca@gmail.com>
2020-11-10 15:04:33 +0000

    challenge 3 completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_delete
 end
